665b9b629659f9ade98326d8a343610ffca6bf2d
mmahbubr0823 <mahbubfdtc@gmail.comm>
2023-10-07 00:37:37 +0600

    routes are set up

--- src/MainLayout/MainLayout.jsx
+++ src/MainLayout/MainLayout.jsx
@@ -1,11 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
 
 const MainLayout = () => {
     return (
-        <div className="max-w-screen-xl mx-auto px-8 md:px-12 lg:px-16 py-2 my-8">
+        <div>
+            <div className="max-w-screen-xl mx-auto px-8 md:px-12 lg:px-16 py-2 my-8">
             <Navbar></Navbar>
             <Outlet></Outlet>
+        </div>
+        <div>
+            <Footer></Footer>
+        </div>
         </div>
     );
 };
